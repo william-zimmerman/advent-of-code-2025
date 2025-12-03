cc7435f80cd37612d549176dbcb779bbd5212702
William Zimmerman <will.zimmerman@gmail.com>
2025-12-03 12:50:45 +0000

    Done with part 1

--- advent-of-go/day3/day3.go
+++ advent-of-go/day3/day3.go
@@ -1,8 +1,78 @@
 package day3
 
-import "fmt"
+import (
+	"cmp"
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+
+	"github.com/samber/lo"
+)
+
+type battery struct {
+	index   int
+	joltage int
+}
+type batteryBank []battery
 
 func Run() (int, error) {
-	fmt.Println("Hello Day3!")
-	return 0, nil
+	bytes, error := os.ReadFile("day3/input.txt")
+
+	if error != nil {
+		return 0, error
+	}
+
+	batteryBanks, error := parseInput(strings.Fields(string(bytes)))
+
+	if error != nil {
+		return 0, error
+	}
+
+	maxVoltages := lo.Map(batteryBanks, func(bBank batteryBank, _ int) int {
+		return maxJoltage(bBank)
+	})
+
+	fmt.Println(maxVoltages)
+
+	answer := lo.Sum(maxVoltages)
+
+	return answer, nil
+}
+
+func parseInput(strings []string) ([]batteryBank, error) {
+	batteryBanks := []batteryBank{}
+	for _, str := range strings {
+		batteryBank := batteryBank{}
+		for index, rune := range str {
+			joltage, error := strconv.Atoi(string(rune))
+
+			if error != nil {
+				return nil, error
+			}
+
+			batteryBank = append(batteryBank, battery{index, joltage})
+		}
+		batteryBanks = append(batteryBanks, batteryBank)
+	}
+
+	return batteryBanks, nil
+}
+
+func maxJoltage(bBank batteryBank) int {
+
+	allButFinalBattery := bBank[:len(bBank)-1]
+
+	tensDigitBattery := slices.MaxFunc(allButFinalBattery, func(b1, b2 battery) int {
+		return cmp.Compare(b1.joltage, b2.joltage)
+	})
+
+	onesDigitCandidates := bBank[tensDigitBattery.index+1:]
+
+	onesDigitBattery := slices.MaxFunc(onesDigitCandidates, func(b1, b2 battery) int {
+		return cmp.Compare(b1.joltage, b2.joltage)
+	})
+
+	return (tensDigitBattery.joltage * 10) + onesDigitBattery.joltage
 }
--- advent-of-go/main.go
+++ advent-of-go/main.go
@@ -10,7 +10,7 @@ func main() {
 	answer, error := day3.Run()
 
 	if error != nil {
-		fmt.Fprintf(os.Stderr, "Encountered error: %s\n", error.Error())
+		fmt.Fprintf(os.Stderr, "Encountered error: %v\n", error)
 	} else {
 		fmt.Println(answer)
 	}
