a02989dac4d2aebf39c330e0316ea8ed1ec18f0b
William Zimmerman <will.zimmerman@gmail.com>
2025-12-04 20:16:40 +0000

    Done with part 1

--- advent-of-go/day4/day4.go
+++ advent-of-go/day4/day4.go
@@ -1,6 +1,102 @@
 package day4
 
+import (
+	"iter"
+	"maps"
+	"os"
+	"slices"
+	"strings"
+)
+
+type coordinate struct {
+	x, y int
+}
+
+type puzzleMap map[coordinate]string
+
+func (p puzzleMap) isPaper(key coordinate) bool {
+	v, found := p[key]
+
+	return found && (v == paper)
+}
+
+const (
+	empty = "."
+	paper = "@"
+)
+
 func Run() (int, error) {
 
-	return 0, nil
+	bytes, error := os.ReadFile("day4/input.txt")
+
+	if error != nil {
+		return 0, error
+	}
+
+	puzzleMap := parseInput(strings.Fields(string(bytes)))
+
+	isPaper := func(c coordinate) bool {
+		return puzzleMap.isPaper(c)
+	}
+
+	accessibleRollsOfPaper := 0
+	for coordinate := range filter(maps.Keys(puzzleMap), isPaper) {
+		paperNeighborCount := len(slices.Collect(filter(coordinate.neighbors(), isPaper)))
+		if paperNeighborCount < 4 {
+			accessibleRollsOfPaper++
+		}
+
+	}
+
+	return accessibleRollsOfPaper, nil
+}
+
+func parseInput(lines []string) puzzleMap {
+
+	puzzleMap := puzzleMap{}
+
+	for y, line := range lines {
+		for x, char := range line {
+			c := coordinate{x: x, y: y}
+			puzzleMap[c] = string(char)
+		}
+	}
+
+	return puzzleMap
+}
+
+func (c coordinate) neighbors() iter.Seq[coordinate] {
+	return func(yield func(coordinate) bool) {
+		for _, neighbor := range []coordinate{c.north(), c.north().east(), c.east(), c.south().east(), c.south(), c.south().west(), c.west(), c.north().west()} {
+			if !yield(neighbor) {
+				return
+			}
+		}
+	}
+}
+
+func filter(items iter.Seq[coordinate], predicate func(c coordinate) bool) iter.Seq[coordinate] {
+	return func(yield func(coordinate) bool) {
+		for item := range items {
+			if predicate(item) && !yield(item) {
+				return
+			}
+		}
+	}
+}
+
+func (c coordinate) north() coordinate {
+	return coordinate{c.x, c.y - 1}
+}
+
+func (c coordinate) east() coordinate {
+	return coordinate{c.x + 1, c.y}
+}
+
+func (c coordinate) south() coordinate {
+	return coordinate{c.x, c.y + 1}
+}
+
+func (c coordinate) west() coordinate {
+	return coordinate{c.x - 1, c.y}
 }
