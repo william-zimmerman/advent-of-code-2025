e778ae80c6701cc1a8ac36eafc9a6b6464b9697a
William Zimmerman <will.zimmerman@gmail.com>
2025-12-05 15:52:39 +0000

    Finished with part 2

--- advent-of-go/day5/day5.go
+++ advent-of-go/day5/day5.go
@@ -2,18 +2,64 @@ package day5
 
 import (
 	"cmp"
+	"iter"
 	"os"
 	"slices"
 	"strconv"
 	"strings"
+
+	"github.com/samber/lo"
 )
 
 type freshIngredientRange struct {
 	from, to ingedientId
 }
 
+func (f freshIngredientRange) all() iter.Seq[ingedientId] {
+	return func(yield func(ingedientId) bool) {
+		for i := f.from; i <= f.to; i++ {
+			if !yield(i) {
+				return
+			}
+		}
+	}
+}
+
 func (f freshIngredientRange) contains(id ingedientId) bool {
 	return f.from <= id && f.to >= id
+}
+
+func (f freshIngredientRange) len() int {
+	return int(f.to) - int(f.from) + 1
+}
+
+func (this freshIngredientRange) overlapsWith(that freshIngredientRange) bool {
+	ranges := []freshIngredientRange{this, that}
+	slices.SortFunc(ranges, func(f1, f2 freshIngredientRange) int {
+		return cmp.Compare(f1.from, f2.from)
+	})
+
+	return ranges[1].from <= ranges[0].to
+}
+
+func (this freshIngredientRange) merge(that freshIngredientRange) freshIngredientRange {
+	return freshIngredientRange{min(this.from, that.from), max(this.to, that.to)}
+}
+
+func min(i1, i2 ingedientId) ingedientId {
+	if i1 <= i2 {
+		return i1
+	}
+
+	return i2
+}
+
+func max(i1, i2 ingedientId) ingedientId {
+	if i1 >= i2 {
+		return i1
+	}
+
+	return i2
 }
 
 type ingedientId int
@@ -25,24 +71,23 @@ func Run() (int, error) {
 		return 0, error
 	}
 
-	freshIngredients, ingredientIds, error := parseInput(strings.Fields(string(bytes)))
+	freshIngredientRanges, _, error := parseInput(strings.Fields(string(bytes)))
 
 	if error != nil {
 		return 0, error
 	}
 
-	slices.SortFunc(freshIngredients, func(a, b freshIngredientRange) int {
+	slices.SortFunc(freshIngredientRanges, func(a, b freshIngredientRange) int {
 		return cmp.Compare(a.from, b.from)
 	})
 
-	freshIngredientCount := 0
-	for _, ingredientId := range ingredientIds {
-		if isFresh(freshIngredients, ingredientId) {
-			freshIngredientCount++
-		}
-	}
+	mergedRanges := merge(freshIngredientRanges)
 
-	return freshIngredientCount, nil
+	lengths := lo.Map(mergedRanges, func(f freshIngredientRange, _ int) int {
+		return f.len()
+	})
+
+	return lo.Sum(lengths), nil
 }
 
 func parseInput(lines []string) ([]freshIngredientRange, []ingedientId, error) {
@@ -93,4 +138,26 @@ func isFresh(freshIngredientsSorted []freshIngredientRange, ingredientId ingedie
 	}
 
 	return false
+}
+
+func merge(ranges []freshIngredientRange) []freshIngredientRange {
+	// assume ranges is sorted
+
+	mergedRanges := []freshIngredientRange{}
+
+	currentRange := ranges[0]
+
+	for i := 1; i < len(ranges); i++ {
+		nextRange := ranges[i]
+		if currentRange.overlapsWith(nextRange) {
+			currentRange = currentRange.merge(nextRange)
+		} else {
+			mergedRanges = append(mergedRanges, currentRange)
+			currentRange = nextRange
+		}
+	}
+
+	mergedRanges = append(mergedRanges, currentRange)
+
+	return mergedRanges
 }
